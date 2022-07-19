450f18e1abf443148a91fdf2d03a68cf66967c00
liubingxiao <1411897519@qq.com>
2022-07-19 09:41:23 +0800

    liubingxiao first commit hot -fix

--- day01/src/helloTest.java
+++ day01/src/helloTest.java
@@ -6,5 +6,7 @@ public class helloTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git1!");
+        System.out.println("hello,git1!,hot-fix");
+
     }
 }
