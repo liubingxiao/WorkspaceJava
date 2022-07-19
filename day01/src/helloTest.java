938af6960d1dde11f2c271e33aa6781828f96638
liubingxiao <1411897519@qq.com>
2022-07-19 09:32:31 +0800

    liubingxiao first commit

---
+++ day01/src/helloTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author lzlinxiao
+ * @create 2022-07-19 9:25
+ */
+public class helloTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
